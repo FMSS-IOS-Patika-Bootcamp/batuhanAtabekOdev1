fc6559d2b17e0705f1d057f66c99ec62de1f57e2
batuhanatabek1 <batuhanatabek@gmail.com>
2022-09-08 19:01:45 +0300

    Initial Commit

---
+++ PatikaOdev1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PatikaOdev1
+//
+//  Created by Batuhan Atabek on 8.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
